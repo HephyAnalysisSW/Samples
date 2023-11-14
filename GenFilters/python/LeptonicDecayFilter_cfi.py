71b06d2770fc4b433fc2ed7bf6df293d18a5a78f
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2023-11-14 15:33:57 +0100

    ifx

--- GenFilters/python/LeptonicDecayFilter_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-leptonicDecayFilter = cms.EDFilter("LeptonicDecayFilter",
-    src = cms.InputTag("source"),
-    #src = cms.InputTag("externalLHEProducer"),
-    minLeptonicWs = cms.int32(0),
-    minLeptonicZs = cms.int32(0),
-    minPt = cms.double(0.),
-)   
